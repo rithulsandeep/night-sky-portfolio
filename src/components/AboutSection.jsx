cb97c2e9bbe2550ded3db9870ab2d484d908408c
rithulsandeep <imrithulsandeep@gmail.com>
2025-08-21 17:05:16 +0530

    Resume update

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -34,7 +34,7 @@ export const AboutSection = () => {
                             </a>
 
                             <a
-                                href="../public/resume/RithulSandeepResume.pdf" target="__blank"
+                                href="../public/resume/MyResume.pdf" target="__blank"
                                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
                             >
                                 View CV
