53cb963e67d76582140a3069e816858dcf382a7b
rithulsandeep <imrithulsandeep@gmail.com>
2025-08-21 17:09:18 +0530

    Add MyResume.pdf to public/resume

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -34,7 +34,8 @@ export const AboutSection = () => {
                             </a>
 
                             <a
-                                href="../public/resume/MyResume.pdf" target="__blank"
+                                href="/resume/MyResume.pdf"
+                                target="_blank"
                                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
                             >
                                 View CV
